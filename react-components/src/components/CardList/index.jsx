864fbcc03f9f30fc8561682ba41f09c5adf631de
riquelme <riquelmebandeira1@outlook.com>
2022-12-18 20:57:55 -0300

    refactor: pass server data by render method args

--- react-components/src/components/CardList/index.jsx
+++ react-components/src/components/CardList/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'; // ES6
+import PropTypes from 'prop-types';
 import Card from '../Card'
 import './styles.scss'
 
--- react-components/src/pages/Home/index.jsx
+++ react-components/src/pages/Home/index.jsx
@@ -1,15 +1,27 @@
-/* eslint-disable no-undef */
 import React from 'react'
+import PropTypes from 'prop-types'
 import CardList from '../../components/CardList'
 import Header from '../../components/Header'
 
-const Home = () => {
+const Home = ({list = []}) => {
  return (
   <>
     <Header />
-    <CardList list={petsFromServer}/>
+    <CardList list={list}/>
   </>
  )
+}
+
+Home.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    type: PropTypes.string,
+    breed: PropTypes.string,
+    picture: PropTypes.string,
+    ownerId: PropTypes.number
+  }))
 }
 
 export default Home
