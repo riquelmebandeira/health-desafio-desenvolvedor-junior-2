f0542a71e02caa35dc3efc63fcf84790285c70e2
riquelme <riquelmebandeira1@outlook.com>
2022-12-18 10:45:43 -0300

    fix: add correct key to sequelize config file

--- server/src/database/config/config.js
+++ server/src/database/config/config.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    url: process.env.DEV_DATABASE_URL,
+    use_env_variable: 'DEV_DATABASE_URL',
     dialect: 'postgres'
   },
   test: {
-    url: process.env.TEST_DATABASE_URL,
+    use_env_variable: 'TEST_DATABASE_URL',
     dialect: 'postgres'
   },
   production: {
-    url: process.env.DATABASE_URL,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres'
   }
 }
