84bb20821557a76f21c37e98bd2bafe9bf314c4c
riquelme <riquelmebandeira1@outlook.com>
2022-12-18 20:39:20 -0300

    feat: add card list component

---
+++ react-components/src/components/CardList/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'; // ES6
+import Card from '../Card'
+import './styles.scss'
+
+const CardList = ({list}) => {
+ return (
+   <section className="card-list">
+     {
+       list.length > 0 && (
+         list.map((pet) => (
+           <Card key={pet.id} {...pet}/>
+         ))
+       )
+     }
+   </section>
+ )
+}
+
+CardList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    age: PropTypes.number,
+    type: PropTypes.string,
+    breed: PropTypes.string,
+    picture: PropTypes.string,
+    ownerId: PropTypes.number
+  }))
+}
+
+export default CardList
